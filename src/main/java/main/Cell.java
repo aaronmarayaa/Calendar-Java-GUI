43760f08f3b37d84b09a7244e574fdf508b8f2d3
Aaron Maraya <aaronmaraya7@gmail.com>
2023-04-26 15:15:45 +0800

    First commit

---
+++ src/main/java/main/Cell.java
@@ -0,0 +1,34 @@
+package main;
+
+
+import java.awt.Color;
+import java.util.Calendar;
+import java.util.Date;
+import javax.swing.JButton;
+
+public class Cell extends JButton{
+    
+    private Date date;
+    private boolean title;
+    Calendar calendar = Calendar.getInstance();
+    
+    public Cell(){
+        setContentAreaFilled(false);
+    }
+    public void asTitle(){
+        title = true;
+    }
+    public boolean isTitle(){
+        return title;
+    }
+    public void setDate(Date date){
+        this.date = date;
+    }
+    public void currentMonth(boolean daysOfCurrentMonth){
+        if(daysOfCurrentMonth){
+            setForeground(new Color(0, 0, 0));
+        }
+        else
+            setForeground(new Color(169,169, 169));
+    }
+}
---
+++ src/main/java/main/PanelDate.java
@@ -0,0 +1,243 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.util.Calendar;
+
+public class PanelDate extends javax.swing.JLayeredPane {
+    
+    private int month;
+    private int year;
+    
+    public PanelDate(int month, int year) {
+        this.month = month;
+        this.year = year;
+        
+        initComponents();
+        init();
+    }
+    private void init(){
+        sun.asTitle();
+        mon.asTitle();
+        tue.asTitle();
+        wed.asTitle();
+        thu.asTitle();
+        fri.asTitle();
+        sat.asTitle();
+        setDate();
+    }
+    public void setDate(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month - 1);  //  month jan as 0 so start from 0
+        calendar.set(Calendar.DATE, 1);
+        int startDay = calendar.get(Calendar.DAY_OF_WEEK) - 1;  //  get day of week -1 to index
+        calendar.add(Calendar.DATE, -startDay);
+        for (Component com : getComponents()) {
+            Cell cell = (Cell) com;
+            if (!cell.isTitle()) {
+                cell.setText(calendar.get(Calendar.DATE) + "");
+                cell.setDate(calendar.getTime());
+                cell.currentMonth(calendar.get(Calendar.MONTH) == month - 1);
+                calendar.add(Calendar.DATE, 1); //  up 1 day
+                Calendar currentSystemDate = Calendar.getInstance();
+                if(calendar.get(Calendar.YEAR) == currentSystemDate.get(Calendar.YEAR) &&
+                    calendar.get(Calendar.MONTH) == currentSystemDate.get(Calendar.MONTH) &&
+                    calendar.get(Calendar.DATE) == currentSystemDate.get(Calendar.DATE) + 1){
+                    cell.setForeground(Color.RED);
+                }
+            }
+        }
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        sun = new main.Cell();
+        mon = new main.Cell();
+        tue = new main.Cell();
+        wed = new main.Cell();
+        thu = new main.Cell();
+        fri = new main.Cell();
+        sat = new main.Cell();
+        cell8 = new main.Cell();
+        cell9 = new main.Cell();
+        cell10 = new main.Cell();
+        cell11 = new main.Cell();
+        cell12 = new main.Cell();
+        cell13 = new main.Cell();
+        cell14 = new main.Cell();
+        cell15 = new main.Cell();
+        cell16 = new main.Cell();
+        cell17 = new main.Cell();
+        cell18 = new main.Cell();
+        cell19 = new main.Cell();
+        cell20 = new main.Cell();
+        cell21 = new main.Cell();
+        cell22 = new main.Cell();
+        cell23 = new main.Cell();
+        cell24 = new main.Cell();
+        cell25 = new main.Cell();
+        cell26 = new main.Cell();
+        cell27 = new main.Cell();
+        cell28 = new main.Cell();
+        cell29 = new main.Cell();
+        cell30 = new main.Cell();
+        cell31 = new main.Cell();
+        cell32 = new main.Cell();
+        cell33 = new main.Cell();
+        cell34 = new main.Cell();
+        cell35 = new main.Cell();
+        cell36 = new main.Cell();
+        cell37 = new main.Cell();
+        cell38 = new main.Cell();
+        cell39 = new main.Cell();
+        cell40 = new main.Cell();
+        cell41 = new main.Cell();
+        cell42 = new main.Cell();
+        cell43 = new main.Cell();
+        cell44 = new main.Cell();
+        cell45 = new main.Cell();
+        cell46 = new main.Cell();
+        cell47 = new main.Cell();
+        cell48 = new main.Cell();
+        cell49 = new main.Cell();
+
+        setLayout(new java.awt.GridLayout(7, 7));
+
+        sun.setForeground(new java.awt.Color(234, 89, 89));
+        sun.setText("Sun");
+        sun.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                sunActionPerformed(evt);
+            }
+        });
+        add(sun);
+
+        mon.setText("Mon");
+        add(mon);
+
+        tue.setText("Tue");
+        add(tue);
+
+        wed.setText("Wed");
+        add(wed);
+
+        thu.setText("Thu");
+        thu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                thuActionPerformed(evt);
+            }
+        });
+        add(thu);
+
+        fri.setText("Fri");
+        add(fri);
+
+        sat.setText("Sat");
+        add(sat);
+        add(cell8);
+        add(cell9);
+        add(cell10);
+        add(cell11);
+        add(cell12);
+        add(cell13);
+        add(cell14);
+        add(cell15);
+        add(cell16);
+        add(cell17);
+        add(cell18);
+        add(cell19);
+        add(cell20);
+        add(cell21);
+        add(cell22);
+        add(cell23);
+        add(cell24);
+        add(cell25);
+        add(cell26);
+        add(cell27);
+        add(cell28);
+        add(cell29);
+        add(cell30);
+        add(cell31);
+        add(cell32);
+        add(cell33);
+        add(cell34);
+        add(cell35);
+        add(cell36);
+        add(cell37);
+        add(cell38);
+        add(cell39);
+        add(cell40);
+        add(cell41);
+        add(cell42);
+        add(cell43);
+        add(cell44);
+        add(cell45);
+        add(cell46);
+        add(cell47);
+        add(cell48);
+        add(cell49);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void sunActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sunActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_sunActionPerformed
+
+    private void thuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_thuActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_thuActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private main.Cell cell10;
+    private main.Cell cell11;
+    private main.Cell cell12;
+    private main.Cell cell13;
+    private main.Cell cell14;
+    private main.Cell cell15;
+    private main.Cell cell16;
+    private main.Cell cell17;
+    private main.Cell cell18;
+    private main.Cell cell19;
+    private main.Cell cell20;
+    private main.Cell cell21;
+    private main.Cell cell22;
+    private main.Cell cell23;
+    private main.Cell cell24;
+    private main.Cell cell25;
+    private main.Cell cell26;
+    private main.Cell cell27;
+    private main.Cell cell28;
+    private main.Cell cell29;
+    private main.Cell cell30;
+    private main.Cell cell31;
+    private main.Cell cell32;
+    private main.Cell cell33;
+    private main.Cell cell34;
+    private main.Cell cell35;
+    private main.Cell cell36;
+    private main.Cell cell37;
+    private main.Cell cell38;
+    private main.Cell cell39;
+    private main.Cell cell40;
+    private main.Cell cell41;
+    private main.Cell cell42;
+    private main.Cell cell43;
+    private main.Cell cell44;
+    private main.Cell cell45;
+    private main.Cell cell46;
+    private main.Cell cell47;
+    private main.Cell cell48;
+    private main.Cell cell49;
+    private main.Cell cell8;
+    private main.Cell cell9;
+    private main.Cell fri;
+    private main.Cell mon;
+    private main.Cell sat;
+    private main.Cell sun;
+    private main.Cell thu;
+    private main.Cell tue;
+    private main.Cell wed;
+    // End of variables declaration//GEN-END:variables
+}
