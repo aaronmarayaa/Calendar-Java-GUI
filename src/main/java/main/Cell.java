7de75c0337c3b2b69e44a3e95d169e3a2eee5ca7
Aaron Maraya <aaronmaraya7@gmail.com>
2023-04-28 19:05:30 +0800

    Add Graphic to mark current day
    -Cell.java

--- src/main/java/main/Cell.java
+++ src/main/java/main/Cell.java
@@ -2,6 +2,9 @@ package main;
 
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.util.Date;
 import javax.swing.JButton;
 
@@ -9,6 +12,7 @@ public class Cell extends JButton{
     
     private Date date;
     private boolean title;
+    private boolean isToday;
     
     public Cell(){
         setContentAreaFilled(false);
@@ -28,5 +32,18 @@ public class Cell extends JButton{
         }
         else
             setForeground(new Color(169,169, 169));
+    }
+    public void setToday(boolean isToday){
+        this.isToday = isToday;
+    }
+    @Override
+    protected void paintComponent(Graphics graphics){
+        if(isToday){
+            Graphics2D g2 = (Graphics2D)graphics;
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setColor(new Color(25,70,255));
+            g2.fillRoundRect(25, 10, getWidth()-50, getHeight()-20, 50, 50);
+        }
+        super.paintComponent(graphics);
     }
 }
