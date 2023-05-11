8150ba143d8cdbe372eaceb7277e60914f5e167e
Aaron Maraya <aaronmaraya7@gmail.com>
2023-05-11 15:05:52 +0800

    Add displayTitleBar
    -to show the TitleBar.
    
    -Main.form
    -Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,12 +4,15 @@ import java.awt.Image;
 import java.awt.Toolkit;
 
 public class Main extends javax.swing.JFrame {
+    
+    private final TitleBar TITLEBAR = new TitleBar(this);
 
     public Main() {
         initComponents();
         setIconImage();
+        displayTitleBar1.display(TITLEBAR);
     }
-    
+
     private void setIconImage(){
         Image icon = Toolkit.getDefaultToolkit().getImage("C:/Programming/Netbeans/Calendar/src/main/java/main/icon.png");
         setIconImage(icon);
@@ -21,23 +24,44 @@ public class Main extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextPane1 = new javax.swing.JTextPane();
+        calendarPanel2 = new main.CalendarPanel();
         calendarPanel1 = new main.CalendarPanel();
+        displayTitleBar1 = new main.DisplayTitleBar();
 
         jScrollPane1.setViewportView(jTextPane1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Calendar");
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        setUndecorated(true);
+
+        displayTitleBar1.setBackground(new java.awt.Color(68, 68, 68));
+        displayTitleBar1.setOpaque(false);
+
+        javax.swing.GroupLayout displayTitleBar1Layout = new javax.swing.GroupLayout(displayTitleBar1);
+        displayTitleBar1.setLayout(displayTitleBar1Layout);
+        displayTitleBar1Layout.setHorizontalGroup(
+            displayTitleBar1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        displayTitleBar1Layout.setVerticalGroup(
+            displayTitleBar1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 31, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(calendarPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 938, Short.MAX_VALUE)
+            .addComponent(calendarPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 943, Short.MAX_VALUE)
+            .addComponent(displayTitleBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(calendarPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(displayTitleBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(calendarPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 432, Short.MAX_VALUE))
         );
 
         pack();
@@ -78,6 +102,8 @@ public class Main extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private main.CalendarPanel calendarPanel1;
+    private main.CalendarPanel calendarPanel2;
+    private main.DisplayTitleBar displayTitleBar1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextPane jTextPane1;
     // End of variables declaration//GEN-END:variables
