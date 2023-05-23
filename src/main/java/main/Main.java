8dfb495bd78caea48e02663ea9999040c2218274
Aaron Maraya <aaronmaraya7@gmail.com>
2023-05-23 13:11:32 +0800

    Remove custom title bar and add Flatlaf
    -add Flatlaf to design the JFrame
    title bar
    
    -Main.form
    -Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,26 +1,20 @@
 package main;
 
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 import java.awt.Image;
 import java.awt.Toolkit;
-import javax.swing.JFrame;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main extends javax.swing.JFrame {
         
-    private final TitleBar TITLEBAR = new TitleBar(this);
-    private int currentState;
-
     public Main() {
         initComponents();
         setIconImage();
-        displayTitleBar1.display(TITLEBAR);
-        TITLEBAR.init(Main.this);
     }
-    
-    private int restoreState() {
-        currentState = TITLEBAR.getCurrentState();
-        return currentState;
-    }
-    
+
     private void setIconImage(){
         Image icon = Toolkit.getDefaultToolkit().getImage("C:/Programming/Netbeans/Calendar/src/main/java/main/icon.png");
         setIconImage(icon);
@@ -30,95 +24,44 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        displayTitleBar1 = new main.DisplayTitleBar();
         calendarPanel1 = new main.CalendarPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Calendar");
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setMinimumSize(new java.awt.Dimension(943, 463));
-        setUndecorated(true);
-        addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowDeiconified(java.awt.event.WindowEvent evt) {
-                formWindowDeiconified(evt);
-            }
-        });
-
-        displayTitleBar1.setBackground(new java.awt.Color(68, 68, 68));
-        displayTitleBar1.setOpaque(false);
-
-        javax.swing.GroupLayout displayTitleBar1Layout = new javax.swing.GroupLayout(displayTitleBar1);
-        displayTitleBar1.setLayout(displayTitleBar1Layout);
-        displayTitleBar1Layout.setHorizontalGroup(
-            displayTitleBar1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 943, Short.MAX_VALUE)
-        );
-        displayTitleBar1Layout.setVerticalGroup(
-            displayTitleBar1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 28, Short.MAX_VALUE)
-        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(displayTitleBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(calendarPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(calendarPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 943, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(displayTitleBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, 0)
-                .addComponent(calendarPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 435, Short.MAX_VALUE))
+            .addComponent(calendarPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 417, Short.MAX_VALUE)
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void formWindowDeiconified(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowDeiconified
-        if (restoreState() == JFrame.MAXIMIZED_BOTH) {
-            setExtendedState(JFrame.MAXIMIZED_BOTH);
-        } else {
-            setExtendedState(JFrame.NORMAL);
-        }
-    }//GEN-LAST:event_formWindowDeiconified
-
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
+            UIManager.setLookAndFeel(new FlatMacDarkLaf());
+            
+            //</editor-fold>
+            java.awt.EventQueue.invokeLater(new Runnable() {
+                public void run() {
+                    new LoadingScreen().setVisible(true);
                 }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            });
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new LoadingScreen().setVisible(true);
-            }
-        });
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private main.CalendarPanel calendarPanel1;
-    private main.DisplayTitleBar displayTitleBar1;
     // End of variables declaration//GEN-END:variables
 }
